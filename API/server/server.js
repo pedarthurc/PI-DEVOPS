665741403feaf248ab1b52dce56999bc60dd2491
pedroarthurca <p.arthur.amaro@gmail.com>
2021-11-24 16:39:17 -0300

    ajuste no caminho para primeira rota

--- API/server/server.js
+++ API/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');             //importa o express
 const app = express();                          //declara app invocando o express
 
-app.use('/',require('./route'))                 //midware para endereçamento de arquivos distribuidos no projeto 
+app.use('/',require('./route/route.js'));          //midware para endereçamento de arquivos distribuidos no projeto 
 
 const PORT = 3000;
 app.listen(PORT);
