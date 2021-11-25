dbb7bfeefe8e5a43c5486973d1593094c2e2e6d7
pedroarthurca <p.arthur.amaro@gmail.com>
2021-11-24 23:14:17 -0300

    ajuste na rota /clientes, esquici de salvar

--- API/server/route/route.js
+++ API/server/route/route.js
@@ -11,6 +11,10 @@ router.get('/teste',async function(req,res){
         content: 'Oi mundo',
         date: new Date()
     }]);
-})
+});
+router.get('/clientes',async function(req,res){
+    const clientes = await service.getAll();
+    res.json(clientes);
+});
 
 module.exports = router;
