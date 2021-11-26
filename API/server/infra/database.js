4c8e86e96db83e76f457e7b9f1530de5121590a8
pedroarthurca <p.arthur.amaro@gmail.com>
2021-11-26 02:56:10 -0300

    teste de acesso com ssl

--- API/server/infra/database.js
+++ API/server/infra/database.js
@@ -1,8 +1,5 @@
 //TUDO REFERENTE AO BANCO DE DADOS
 const pgp = require('pg-promise')();
-//teste de ssl -- remover 'fs' se falar
-const fs = require('fs');
-
 console.log('aqui-0');
 const db = pgp({
     host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
@@ -10,14 +7,6 @@ const db = pgp({
     user: 'lwkjswucvoqsio',
     port: '5432',
     password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7',
-    //teste ssl - remover daqui
-    ssl: {
-        rejectUnauthorized : false,
-        ca   : fs.readFileSync("server-ca.pem").toString(),
-        key  : fs.readFileSync("client-key.pem").toString(),
-        cert : fs.readFileSync("client-cert.pem").toString(),
-  }
-  //até aqui
 });
 console.log('aqui-0,5');
 module.exports = db;
