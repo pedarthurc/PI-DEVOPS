24623db755963e3942d870b5cd22f3de994ed488
pedroarthurca <p.arthur.amaro@gmail.com>
2021-11-26 01:30:30 -0300

    verificação das credenciais de acesso ao banco postgres/heroku

--- API/server/infra/database.js
+++ API/server/infra/database.js
@@ -2,11 +2,11 @@
 const pgp = require('pg-promise')();
 console.log('aqui-0');
 const db = pgp({
-    user: 'lwkjswucvoqsio',
-    password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7',
     host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
+    database: 'de0gvkdpmp3tqd',
+    user: 'lwkjswucvoqsio',
     port: '5432',
-    database: 'de0gvkdpmp3tqd'
+    password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7'
 });
 
 module.exports = db;
