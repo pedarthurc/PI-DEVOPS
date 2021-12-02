78465d0e22c42d67da87973fa892ed059d31e8d6
pedroarthurca <p.arthur.amaro@gmail.com>
2021-12-02 18:15:21 -0300

    retorno a estrutura original de credenciais do banco

--- API/server/infra/database.js
+++ API/server/infra/database.js
@@ -16,11 +16,5 @@ module.exports = db;
     port: '5432',
     password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7'
 }),
-const db = pgp({
-    host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
-    database: 'de0gvkdpmp3tqd',
-    user: 'lwkjswucvoqsio',
-    port: '5432',
-    password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7'
-});*/
+*/
 
