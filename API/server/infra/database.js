330f3344da48cf7b8344f605b66d4499cd6343d5
pedroarthurca <p.arthur.amaro@gmail.com>
2021-12-02 18:05:02 -0300

    credenciais do novo banco

--- API/server/infra/database.js
+++ API/server/infra/database.js
@@ -10,11 +10,11 @@ db.heroku() = pgp({
     password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7'
 }),
 db.iknet() = pgp({
-    host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
-    database: 'de0gvkdpmp3tqd',
-    user: 'lwkjswucvoqsio',
+    host: '192.168.2.12',
+    database: 'postgres',
+    user: 'postgres',
     port: '5432',
-    password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7'
+    password: '987654321'
 })
 module.exports = db;
 
