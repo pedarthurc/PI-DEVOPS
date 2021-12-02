41f3648423448c611c244200b293df4a652fea69
pedroarthurca <p.arthur.amaro@gmail.com>
2021-12-02 18:09:43 -0300

    retorno a estrutura original de credenciais do banco

--- API/server/infra/database.js
+++ API/server/infra/database.js
@@ -1,15 +1,6 @@
 //TUDO REFERENTE AO BANCO DE DADOS
 const pgp = require('pg-promise')();
-const db;
-
-db.heroku() = pgp({
-    host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
-    database: 'de0gvkdpmp3tqd',
-    user: 'lwkjswucvoqsio',
-    port: '5432',
-    password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7'
-}),
-db.iknet() = pgp({
+const db = pgp({
     host: '192.168.2.12',
     database: 'postgres',
     user: 'postgres',
@@ -18,7 +9,14 @@ db.iknet() = pgp({
 })
 module.exports = db;
 
-/*const db = pgp({
+/*db.heroku() = pgp({
+    host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
+    database: 'de0gvkdpmp3tqd',
+    user: 'lwkjswucvoqsio',
+    port: '5432',
+    password: 'f011b9f988c1d3bbd37390c1281e50c17ba51d99c940be46415a4aadeb3a8ff7'
+}),
+const db = pgp({
     host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
     database: 'de0gvkdpmp3tqd',
     user: 'lwkjswucvoqsio',
